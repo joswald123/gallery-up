b7694ff5c1b8c91ca70ca12c59c8d2c7083947d8
joswald123 <julianaandreaoswald@gmail.com>
2022-07-07 11:30:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Nav from './components/Nav'
+import axios from 'axios';
+import SearchForm from './components/SearchForm';
+import Photo from './components/Photo';
+import NotFound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    }
+  };
+  
+  
+
+  componentDidMount() {
+    const apiKey = "2ed4de34d46523ba619a652110d5a44f"
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cat&perpage=24&format=json&nojsoncallback=1`)
+    .then((response) => {
+      console.log(response.data.photos.photo)
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    })
+  }
+
+  search
+
+  render() {
+    return (
+      <div className="container">
+        <SearchForm />
+        <Nav />
+        <div className="photo-container">
+        <h2>Results</h2>
+        <ul>
+          <Photo />
+          <NotFound />
+        </ul>
+        </div>
+      </div>
+    );
+  }
+ 
 }
 
 export default App;
